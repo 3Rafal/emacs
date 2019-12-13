3b9c48a17a92fd0707983179e11885b940c5a605
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2019-12-13 18:50:25 +0100

    Add C and Scheme stuff

--- init.el
+++ init.el
@@ -14,7 +14,9 @@
 		     projectile
 		     treemacs
 		     treemacs-evil
-		     treemacs-projectile))
+		     treemacs-projectile
+		     paredit
+		     geiser))
 
 ; Add Melpa as the default Emacs Package repository
 ; only contains a very limited number of packages
@@ -89,6 +91,15 @@
 ; Treemacs
 (global-set-key (kbd "C-x t") 'treemacs)
 
+; Paredit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
 ;; Disable menubar, toolbar and scrollbar
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
@@ -105,6 +116,10 @@
 
 ; Sort apropos by relevancy
 (setq apropos-sort-by-scores t)
+
+; C-style
+(setq c-default-style "linux"
+          c-basic-offset 4)
 
 ; Enable show-paren-mode
 (show-paren-mode 1)
