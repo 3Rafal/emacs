a76de299fb306f1de8cd1ee11f004e76ba549c4e
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2021-02-05 08:48:43 +0100

    further dired tweaks

--- init.el
+++ init.el
@@ -247,14 +247,25 @@
     (setq company-dabbrev-downcase nil))
   :diminish company-mode)
 
+; dired
 (use-package dired
   :ensure nil
   :commands (dired dired-jump)
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
   :bind (("C-x C-j" . dired-jump))
   :config
   (evil-collection-define-key 'normal 'dired-mode-map
     "h" 'dired-up-directory
     "l" 'dired-find-file))
+
+(use-package dired-single)
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "H" 'dired-hide-dotfiles-mode))
 
 ; terms/shells
 
